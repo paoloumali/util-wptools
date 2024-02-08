7c82669469f0b5c0b8647549086ac5ba1c8bedd6
paoloumali <2522851+paoloumali@users.noreply.github.com>
2024-02-08 08:05:32 +0800

    start project

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM php:8.1-alpine
+
+RUN docker-php-ext-install mysqli pdo pdo_mysql && \
+  docker-php-ext-enable mysqli pdo_mysql
+
+RUN apk update && apk add mysql-client mariadb-connector-c-dev \
+  zip bash git
+
+WORKDIR /tmp/wordpress
---
+++ sbin/wp-archive.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+wpdir=/tmp/wordpress
+baksdir=/tmp/backups
+
+timestamp=$(date +%s)
+htime=$(date -d @$timestamp +%m-%d-%Y_%H-%M-%S)
+timename=$timestamp.$htime
+
+## This allows them to rebuild site without wp-content which is mostly big files
+cd /tmp
+git init
+git config --global user.email "dev@paolo.ph"
+git config --global user.name "paoloumali"
+git checkout master
+git add -A
+git commit -m "time: $timename"
+git checkout -b $htime
+
+cd $wpdir
+## db export
+wp db export --allow-root db_$timename.sql
+
+## archive it
+zip -r $baksdir/wp-archive_latest.zip \
+  $wpdir/{.htaccess,wp-config.php,wp-content} \
+  *.sql \
+  /tmp/.git
+
+## delete remnant db
+rm *.sql
+
+
